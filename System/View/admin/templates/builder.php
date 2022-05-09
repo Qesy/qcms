007f2498341f78f8c5501e87d9b24cb4e8d13df7
Qesy <762264@qq.com>
2022-05-09 22:55:58 +0800

    add

--- System/View/admin/templates/builder.php
+++ System/View/admin/templates/builder.php
@@ -663,7 +663,7 @@
 
 <textarea class="form-control text-dark mb-3 p-2" rows="15">
 {{tag Row='10'}}
-    <a href="{{qcms:Tag_Url}}"><img src="{{qcms:Tag_Name}}"/></a>
+    <a href="{{qcms:Tag_Url}}">{{qcms:Tag_Name}}</a>
 {{/tag}}
 </textarea>
                                         <h5 >属性说明</h5>
